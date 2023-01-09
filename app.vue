39f913b7752d54672f1fcd278fb2c8282c3ba43b
Yusniel Brocard <brocard@gmail.com>
2023-01-09 16:34:55 -0600

    wip

--- app.vue
+++ app.vue
@@ -10,6 +10,12 @@ const nuxtApp = useNuxtApp();
 onMounted(async () => {
   loading.value = true;
 });
+
+const selectedDate = ref({
+  start: new Date(2018, 10, 9),
+  end: new Date(2018, 10, 10),
+});
+
 </script>
 
 <template>
@@ -17,10 +23,13 @@ onMounted(async () => {
 
     <div class="app-search max-w-xl mx-auto mt-10">
       <ClientOnly>
-        <template #placeholder>
+        <template #fallback>
           <input class="datepicker"/>
           <span class="-ml-6 mr-2">to</span>
-          <input class="datepicker"/><br/>
+          <input class="datepicker"/>
+          <div class="vc-popover-content-wrapper is-interactive">
+            <div></div>
+          </div>
         </template>
 
         <v-date-picker
@@ -33,10 +42,23 @@ onMounted(async () => {
           <template v-slot="{ inputValue, inputEvents}">
             <input :value="inputValue.start" v-on="inputEvents.start" class="datepicker"/>
             <span class="-ml-6 mr-2">to</span>
-            <input :value="inputValue.end" v-on="inputEvents.end" class="datepicker"/><br/>
+            <input :value="inputValue.end" v-on="inputEvents.end" class="datepicker"/>
           </template>
         </v-date-picker>
       </ClientOnly>
     </div>
+
+    <div>
+      <ClientOnly>
+        <v-date-picker
+          mode="range"
+          is-range
+          v-model="selectedDate"
+          :min-date="new Date()"
+          show-caps
+        />
+      </ClientOnly>
+    </div>
+
   </div>
 </template>
